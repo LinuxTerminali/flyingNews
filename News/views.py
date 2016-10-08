2fecab0c4f459c14997f840cb13895e4f78612d3
LinuxTerminali <nkmishra1995@gmail.org>
2016-10-08 22:02:09 +0530

    Initial commit

---
+++ News/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+import json
+import urllib2
+from django.template import loader
+# Create your views here.
+
+
+def home(self):
+    template = loader.get_template('News/index.html')
+    dic = data()
+    context = {
+        'dico': dic,
+    }
+    #html = ''
+    #for foo in dic['items']:
+    #    html += '<a href = #>' + foo['title'] +</a><br>''
+    return HttpResponse(template.render(context))
+
+
+def data():
+    json_ob = json.load(urllib2.urlopen(
+        'http://127.0.0.1:5000/scraping'))
+    return json_ob
