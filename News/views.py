7d56ce4d0eddc955a3c5dfcf07f8f1a1986555d6
LinuxTerminali <nkmishra1995@gmail.org>
2016-10-09 21:52:48 +0530

    Enhanced ui and bootstrap 4

--- News/views.py
+++ News/views.py
@@ -19,5 +19,5 @@ def home(self):
 
 def data():
     json_ob = json.load(urllib2.urlopen(
-        'http://flyingkites.herokuapp.com/query/tech'))
+        'http://127.0.0.1:5000/scraping'))
     return json_ob
--- flyingNews/settings.py
+++ flyingNews/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f1)pivy41tu7*_fy$8^coia%*c#yyw6l$9%0^y96hk_bl25ske'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
